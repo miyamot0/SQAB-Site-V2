4cd3e382e6f17fc0b6748cf715f5c8abd7453494
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-09-02 22:18:38 -0500

    Move recruitment to firestore hosting

---
+++ src/firebase/useFirebaseCollection.tsx
@@ -0,0 +1,65 @@
+/** @license
+ *
+ * Copyright (c) Shawn P. Gilroy, Louisiana State University.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Firebase collection
+ */
+
+import { useEffect, useState } from 'react';
+import { projectFirestore } from './config';
+
+import { Query } from '@firebase/firestore-types';
+import { RecruitmentAd } from '../pages/recruitment/types/RecruitmentTypes';
+
+const CollectionError = 'Unable to retrieve data';
+
+export type CurrentObjectTypes = RecruitmentAd;
+export type CurrentObjectTypeArrays = RecruitmentAd[];
+
+interface UseFirebaseCollection {
+  documents: CurrentObjectTypeArrays | null;
+  error: string | undefined;
+}
+
+/** useFirebaseCollection
+ *
+ * Access a collection
+ *
+ * @param {string} collectionString collection address
+ * @returns {UseFirebaseCollection}
+ */
+export function useFirebaseCollection(collectionString: string): UseFirebaseCollection {
+  const [documents, setDocuments] = useState<RecruitmentAd[] | null>(null);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    let ref: Query = projectFirestore.collection(collectionString);
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = Array<CurrentObjectTypes | null>();
+
+        snapshot.docs.forEach((doc) => {
+          let preDoc = doc.data() as CurrentObjectTypes;
+          preDoc.id = doc.id;
+          results.push(preDoc);
+        });
+
+        setDocuments(results as RecruitmentAd[]);
+        setError(undefined);
+      },
+      (error) => {
+        setError(CollectionError);
+      },
+    );
+
+    return () => unsubscribe();
+  }, [collectionString]);
+
+  return { documents, error };
+}
---
+++ src/pages/recruitment/types/RecruitmentTypes.tsx
@@ -0,0 +1,17 @@
+/**
+ * Firebase object
+ */
+export type RecruitmentAd = {
+  Bio: string;
+  Contact: string;
+  Cycle: string;
+  Description: string;
+  Institution: string;
+  Link: string;
+  Mentor: string;
+  Name: string;
+  Position: string;
+  PositionText: string;
+  PositionTitle: string;
+  id: string;
+};
