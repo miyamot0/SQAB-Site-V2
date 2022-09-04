3bbbdbf984059447fe7da0903e050277ba62bc11
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-09-04 01:12:01 -0500

    Add in refs

--- src/pages/tools/DiscountingModelSelector.tsx
+++ src/pages/tools/DiscountingModelSelector.tsx
@@ -786,21 +786,21 @@ export default function DiscountingModelSelector(): JSX.Element {
                 <br />
                 Franck, C. T., Koffarnus, M. N., House, L. L., &#38; Bickel, W. K. (2015). Accurate
                 characterization of delay discounting: a multiple model approach using approximate
-                Bayesian model selection and a unified discounting measure. Journal of the
-                Experimental Analysis of Behavior, 103(1), 218-233.
+                Bayesian model selection and a unified discounting measure.{' '}
+                <i>Journal of the Experimental Analysis of Behavior, 103(1)</i>, 218-233.
                 https://doi.org/10.1002/jeab.128.{' '}
                 <a href="https://doi.org/10.1002/jeab.128">doi: 10.1002/jeab.128</a>.
                 <br />
                 <br />
                 Gilroy, S. P., Franck, C. T. &#38; Hantula, D. A. (2017). The discounting model
-                selector: Statistical software for delay discounting applications. Journal of the
-                Experimental Analysis of Behavior, 107(3), 388-401.{' '}
+                selector: Statistical software for delay discounting applications.{' '}
+                <i>Journal of the Experimental Analysis of Behavior, 107(3)</i>, 388-401.{' '}
                 <a href="https://doi.org/10.1002/jeab.257">doi: 10.1002/jeab.257</a>.
                 <br />
                 <br />
                 Gilroy, S. P. &#38; Hantula, D. A. (2018). Discounting model selection with
-                area-based measures: A case for numerical integration. Journal of the Experimental
-                Analysis of Behavior.{' '}
+                area-based measures: A case for numerical integration.{' '}
+                <i>Journal of the Experimental Analysis of Behavior, 109(2)</i>, 433-449.{' '}
                 <a href="https://doi.org/10.1002/jeab.318">doi: 10.1002/jeab.318</a>.
               </MDBCardText>
             </MDBCardBody>
