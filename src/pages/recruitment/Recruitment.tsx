d2ea3147273b0cdb3e72ae3c11a4ed6ddfb22678
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-09-08 14:21:39 -0500

    typo

--- src/pages/recruitment/Recruitment.tsx
+++ src/pages/recruitment/Recruitment.tsx
@@ -18,7 +18,7 @@ export default function Recruitment(): JSX.Element {
           <MDBCard>
             <MDBCardBody>
               <MDBCardTitle>Current and Upcoming Graduate Student Opportunities</MDBCardTitle>
-              <MDBCardText>For those interested in posting an opportunity, please email the current student representative, Tadd Schneider (<a href='mailto:tadd@ku.edu'>tadd@ku.edu</a>), after completing four steps. First, sign in and authenticate to establish an account (see upper right button in the navigation bar). Second, complete your profile so that the student representative can easily identify your account. Third, once you have access to your recruitment call, you will have to contact the student representative and request a review. Lastly, the student representative will approve your ad before it goes live on the site. </MDBCardText>
+              <MDBCardText>For those interested in posting an opportunity, please email the current student representative, Tadd Schneider (<a href='mailto:tadd@ku.edu'>tadd@ku.edu</a>), after reviewing the following steps. First, sign in and authenticate to establish an account (see upper right button in the navigation bar). Second, complete your profile so that the student representative can easily identify your account. Third, once you have access to your recruitment call, you will have to contact the student representative and request a review. Lastly, the student representative will approve your ad before it goes live on the site. </MDBCardText>
               {documents && RecruitmentTable(documents)}
             </MDBCardBody>
           </MDBCard>
