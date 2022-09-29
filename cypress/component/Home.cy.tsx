2420e39041238858cf9d451607c3c754e20d8f05
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-09-29 13:01:30 -0500

    Add in login fx

---
+++ cypress/component/Home.cy.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import App from "../../src/App"
+
+describe('Home.cy.tsx', () => {
+  it('playground', () => {
+    //cy.mount(<App />)
+  })
+})
---
+++ cypress/support/component.ts
@@ -0,0 +1,62 @@
+// ***********************************************************
+// This example support/component.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands';
+import { mount } from 'cypress/react';
+import { projectAuth } from '../../src/firebase/config';
+import { email, password } from './../fixtures/auth-user';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      login(email: any, password: any): void;
+      logout(): void;
+      saveLocalStorage(): void;
+      restoreLocalStorage(): void;
+      mount: typeof mount;
+    }
+  }
+}
+
+// eslint-disable-next-line prefer-const
+let LOCAL_STORAGE_MEMORY: any = {};
+
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+
+Cypress.Commands.add('mount', mount);
+
+Cypress.Commands.add('login', () => {
+  return projectAuth.signInWithEmailAndPassword(email, password);
+});
+
+Cypress.Commands.add('logout', () => {
+  return projectAuth.signOut();
+});
+
+Cypress.Commands.add('saveLocalStorage', () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add('restoreLocalStorage', () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
