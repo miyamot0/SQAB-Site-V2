6efd2b4dbd6ab5c6f043229bbcf7d5060f20bb95
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-10-01 16:11:13 -0500

    Migrate all binaries to storage

--- src/pages/records/Records.tsx
+++ src/pages/records/Records.tsx
@@ -7,187 +7,150 @@
  */
 
 import React from 'react';
-
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from 'mdb-react-ui-kit';
-
 import { CardBodyTextStyle } from '../../utilities/StyleHelper';
-
-import Program2004 from './../../assets/programs/2004.pdf';
-import Program2005 from './../../assets/programs/2005.pdf';
-import Program2006 from './../../assets/programs/2006.pdf';
-import Program2007 from './../../assets/programs/2007.pdf';
-// TODO: need 2008
-import Program2009 from './../../assets/programs/2009.pdf';
-import Program2010 from './../../assets/programs/2010.pdf';
-import Program2011 from './../../assets/programs/2011.pdf';
-import Program2012 from './../../assets/programs/2012.pdf';
-import Program2013 from './../../assets/programs/2013.pdf';
-import Program2014 from './../../assets/programs/2014.pdf';
-import Program2015 from './../../assets/programs/2015.pdf';
-import Program2016 from './../../assets/programs/2016.pdf';
-import Program2017 from './../../assets/programs/2017.pdf';
-import Program2018 from './../../assets/programs/2018.pdf';
-import Program2019 from './../../assets/programs/2019.pdf';
-import Program2020 from './../../assets/programs/2020-contact.pdf';
-import Program2021 from './../../assets/programs/2021.pdf';
-import Program2022 from './../../assets/programs/2022.pdf';
-
-import Newsletter2005 from './../../assets/newsletters/2005.pdf';
-import Newsletter2006 from './../../assets/newsletters/2006.pdf';
-// TODO: need 2007
-import Newsletter2008 from './../../assets/newsletters/2008.pdf';
-import Newsletter2009 from './../../assets/newsletters/2009.pdf';
-// TODO: need 2010
-import Newsletter2011 from './../../assets/newsletters/2011.pdf';
-import Newsletter2012 from './../../assets/newsletters/2012.pdf';
-import Newsletter2013 from './../../assets/newsletters/2013.pdf';
-import Newsletter2014 from './../../assets/newsletters/2014.pdf';
-import Newsletter2015 from './../../assets/newsletters/2015.pdf';
-import Newsletter2016 from './../../assets/newsletters/2016.pdf';
-import Newsletter2017 from './../../assets/newsletters/2017.pdf';
-import Newsletter2018 from './../../assets/newsletters/2018.pdf';
-import Newsletter2019 from './../../assets/newsletters/2019.pdf';
-import Newsletter2020 from './../../assets/newsletters/2020.pdf';
-// TODO: need 2021
-import Newsletter2022 from './../../assets/newsletters/2022.pdf';
 
 import './styles/Records.css';
 
 const Programs = [
   {
     Year: 2004,
-    Path: Program2004,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2004.pdf?alt=media&token=5a5059b1-555c-40ef-9e7d-7f479a97bdf6',
   },
   {
     Year: 2005,
-    Path: Program2005,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2005.pdf?alt=media&token=bfeeb5cf-5e9f-4722-a2df-cdc956e8257c',
   },
   {
     Year: 2006,
-    Path: Program2006,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2006.pdf?alt=media&token=c9d8d607-5661-48e4-a342-4695d4753cb1',
   },
   {
     Year: 2007,
-    Path: Program2007,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2007.pdf?alt=media&token=7a059df5-2673-4584-b8d0-aad9f3ffd287',
   },
   {
     Year: 2009,
-    Path: Program2009,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2009.pdf?alt=media&token=34c89a80-7550-4269-bd38-7958c5538f49',
   },
   {
     Year: 2010,
-    Path: Program2010,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2010.pdf?alt=media&token=19246e13-32f4-4294-97ba-8bef991b348c',
   },
   {
     Year: 2011,
-    Path: Program2011,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2011.pdf?alt=media&token=0f2e5101-750d-4ae5-a988-42182e562011',
   },
   {
     Year: 2012,
-    Path: Program2012,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2012.pdf?alt=media&token=64f46d30-63eb-4b52-9921-f11887bfd82d',
   },
   {
     Year: 2013,
-    Path: Program2013,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2013.pdf?alt=media&token=142dd38f-c2af-44c8-94e9-c7d0ed5ac70a',
   },
   {
     Year: 2014,
-    Path: Program2014,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2014.pdf?alt=media&token=647c9df6-70fa-4bfd-89fe-3c785d7d8898',
   },
   {
     Year: 2015,
-    Path: Program2015,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2015.pdf?alt=media&token=a99eae43-6067-4fc1-8715-3b242efb89d8',
   },
   {
     Year: 2016,
-    Path: Program2016,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2016.pdf?alt=media&token=613a541a-12a8-4ec9-be11-99134cbf0050',
   },
   {
     Year: 2017,
-    Path: Program2017,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2017.pdf?alt=media&token=47e4f19f-0395-4dd3-9c06-f0440dbcf2ed',
   },
   {
     Year: 2018,
-    Path: Program2018,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2018.pdf?alt=media&token=9e8424a5-00ea-4d8b-ab4b-13baa3008c11',
   },
   {
     Year: 2019,
-    Path: Program2019,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2019.pdf?alt=media&token=e365f3d7-034e-4fd6-addb-df6db7f8c5d6',
   },
   {
     Year: 2020,
-    Path: Program2020,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2020-contact.pdf?alt=media&token=46511323-8861-4c83-a474-fd8360aaa433',
   },
   {
     Year: 2021,
-    Path: Program2021,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2021.pdf?alt=media&token=189ef144-b8fa-4a39-a898-9a05ccaef76b',
   },
   {
     Year: 2022,
-    Path: Program2022,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/programs%2F2022.pdf?alt=media&token=d22dfbda-6270-4054-9321-4e77d814b5a2',
   },
 ];
 
 const Newsletters = [
   {
     Year: 2005,
-    Path: Newsletter2005,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2005.pdf?alt=media&token=42b90474-7514-4ed1-b152-f9068048b422',
   },
   {
     Year: 2006,
-    Path: Newsletter2006,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2006.pdf?alt=media&token=410cd77d-7971-4216-b341-abbb165e24a7',
   },
   {
     Year: 2008,
-    Path: Newsletter2008,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2008.pdf?alt=media&token=4f25a56a-22e4-4696-89de-1dbc89dba22f',
   },
   {
     Year: 2009,
-    Path: Newsletter2009,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2009.pdf?alt=media&token=2090e98a-14f9-4c67-96f1-0114ac68380c',
   },
   {
     Year: 2011,
-    Path: Newsletter2011,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2011.pdf?alt=media&token=1d478dbe-ad5f-4b32-b191-efbbd549b63a',
   },
   {
     Year: 2012,
-    Path: Newsletter2012,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2012.pdf?alt=media&token=1b6446fb-feb7-4fd2-9675-021451881448',
   },
   {
     Year: 2013,
-    Path: Newsletter2013,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2013.pdf?alt=media&token=0659dfdb-d6dc-43a9-9185-58d90b9e15b8',
   },
   {
     Year: 2014,
-    Path: Newsletter2014,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2014.pdf?alt=media&token=c3b347de-1a9b-4297-b255-54c1bd747631',
   },
   {
     Year: 2015,
-    Path: Newsletter2015,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2015.pdf?alt=media&token=367d0399-ecb8-4cab-bb74-797d9bd32156',
   },
   {
     Year: 2016,
-    Path: Newsletter2016,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2016.pdf?alt=media&token=3ac6c837-1c91-4c66-8517-80ed804471f6',
   },
   {
     Year: 2017,
-    Path: Newsletter2017,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2017.pdf?alt=media&token=6a87b3ce-2330-421a-893c-22621c227c7c',
   },
   {
     Year: 2018,
-    Path: Newsletter2018,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2018.docx?alt=media&token=3bbe9924-c377-47f0-94e9-3dce462b9539',
+  },
+  {
+    Year: 2018,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2018.pdf?alt=media&token=13f0ee55-a764-472c-bb85-fc3b33bcb577',
   },
   {
     Year: 2019,
-    Path: Newsletter2019,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2019.pdf?alt=media&token=daae9528-7366-431c-9709-5281b166a0fb',
   },
   {
     Year: 2020,
-    Path: Newsletter2020,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2020.pdf?alt=media&token=90db399f-04e6-4db2-b717-a717d6cd77c7',
   },
   {
     Year: 2022,
-    Path: Newsletter2022,
+    Path: 'https://firebasestorage.googleapis.com/v0/b/sqab-website-v2.appspot.com/o/newletters%2F2022.pdf?alt=media&token=2e1ab1a6-5604-4264-9637-c4ecafe3ad58',
   },
 ];
 
