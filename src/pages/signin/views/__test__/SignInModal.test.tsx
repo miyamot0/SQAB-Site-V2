5119c52098f8c28f5f9f1b021d9daaf695437fbe
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-10-06 23:21:41 -0500

    minor progress

--- src/pages/signin/views/__test__/SignInModal.test.tsx
+++ src/pages/signin/views/__test__/SignInModal.test.tsx
@@ -6,99 +6,205 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
+import React, { ReactNode, ReactPortal } from 'react';
 import Modal from 'react-modal';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { act } from '@testing-library/react';
+import { act, fireEvent, render } from '@testing-library/react';
 import { SignInModal } from '../SignInModal';
+import ReactDOM from 'react-dom';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('../../../../firebase/hooks/useFirebaseLogin', () => {
-    return {
-        ...jest.requireActual('../../../../firebase/hooks/useFirebaseLogin'),
-        login: jest.fn(() => true),
-    }
-})
+  return {
+    ...jest.requireActual('../../../../firebase/hooks/useFirebaseLogin'),
+    login: jest.fn(() => true),
+  };
+});
 
 describe('SignInModal', () => {
-    it('On load, nothing shown', async () => {
-        const showModal = false;
-        const showPhoneNumber = false;
-        const showOTP = false;
+  it('On load, nothing shown', async () => {
+    const showModal = false;
+    const showPhoneNumber = false;
+    const showOTP = false;
 
-        const setShowModal = jest.fn();
-        const setPhoneNumber = jest.fn();
-        const setOTPNumber = jest.fn();
-        const setShowPhoneNumber = jest.fn();
-        const setConfirmResult = jest.fn();
-        const setShowOTP = jest.fn();
+    const setShowModal = jest.fn();
+    const setPhoneNumber = jest.fn();
+    const setOTPNumber = jest.fn();
+    const setShowPhoneNumber = jest.fn();
+    const setConfirmResult = jest.fn();
+    const setShowOTP = jest.fn();
 
-        const login = jest.fn();
-        const confirmResult = jest.fn();
+    const login = jest.fn();
+    const confirmResult = jest.fn();
 
-        await act(async () => {
-            const wrapper = shallow(
-                <SignInModal showModal={showModal} showPhoneNumber={showPhoneNumber} showOTP={showOTP}
-                    setShowModal={setShowModal} setPhoneNumber={setPhoneNumber} setOTPNumber={setOTPNumber}
-                    setShowPhoneNumber={setShowPhoneNumber} setConfirmResult={setConfirmResult} setShowOTP={setShowOTP}
-                    phoneNumber={''} otpNumber={''} login={login} confirmResult={confirmResult} />,
-            );
+    await act(async () => {
+      const wrapper = shallow(
+        <SignInModal
+          showModal={showModal}
+          showPhoneNumber={showPhoneNumber}
+          showOTP={showOTP}
+          setShowModal={setShowModal}
+          setPhoneNumber={setPhoneNumber}
+          setOTPNumber={setOTPNumber}
+          setShowPhoneNumber={setShowPhoneNumber}
+          setConfirmResult={setConfirmResult}
+          setShowOTP={setShowOTP}
+          phoneNumber={''}
+          otpNumber={''}
+          login={login}
+          confirmResult={confirmResult}
+        />,
+      );
 
-            //wrapper.find('button').first().simulate('click');
-        });
-    })
+      //wrapper.find('button').first().simulate('click');
+    });
+  });
 
-    it('On load, all shown', async () => {
-        const showModal = true;
-        const showPhoneNumber = true;
-        const showOTP = true;
+  it('On load, all shown', async () => {
+    const showModal = true;
+    const showPhoneNumber = true;
+    const showOTP = true;
 
-        const setShowModal = jest.fn();
-        const setPhoneNumber = jest.fn();
-        const setOTPNumber = jest.fn();
-        const setShowPhoneNumber = jest.fn();
-        const setConfirmResult = jest.fn();
-        const setShowOTP = jest.fn();
+    const setShowModal = jest.fn();
+    const setPhoneNumber = jest.fn();
+    const setOTPNumber = jest.fn();
+    const setShowPhoneNumber = jest.fn();
+    const setConfirmResult = jest.fn();
+    const setShowOTP = jest.fn();
 
-        const login = jest.fn();
-        const confirmResult = jest.fn();
+    const login = jest.fn();
+    const confirmResult = jest.fn();
 
-        await act(async () => {
-            const wrapper = shallow(
-                <SignInModal showModal={showModal} showPhoneNumber={showPhoneNumber} showOTP={showOTP}
-                    setShowModal={setShowModal} setPhoneNumber={setPhoneNumber} setOTPNumber={setOTPNumber}
-                    setShowPhoneNumber={setShowPhoneNumber} setConfirmResult={setConfirmResult} setShowOTP={setShowOTP}
-                    phoneNumber={''} otpNumber={''} login={login} confirmResult={confirmResult} />,
-            );
+    await act(async () => {
+      const wrapper = shallow(
+        <SignInModal
+          showModal={showModal}
+          showPhoneNumber={showPhoneNumber}
+          showOTP={showOTP}
+          setShowModal={setShowModal}
+          setPhoneNumber={setPhoneNumber}
+          setOTPNumber={setOTPNumber}
+          setShowPhoneNumber={setShowPhoneNumber}
+          setConfirmResult={setConfirmResult}
+          setShowOTP={setShowOTP}
+          phoneNumber={''}
+          otpNumber={''}
+          login={login}
+          confirmResult={confirmResult}
+        />,
+      );
 
-            const inputs = wrapper.find('input');
+      const inputs = wrapper.find('input');
 
-            expect(inputs.length).toBe(1)
-            inputs.first().simulate('change', { target: { value: 'Hello' } })
-            expect(setOTPNumber).toBeCalled();
+      expect(inputs.length).toBe(1);
+      inputs.first().simulate('change', { target: { value: 'Hello' } });
+      expect(setOTPNumber).toBeCalled();
 
-            const mainModal = wrapper.find(Modal).dive();
-            const mainModalBtns = mainModal.find('.btn')
+      const mainModal = wrapper.find(Modal).dive();
+      const mainModalBtns = mainModal.find('.btn');
 
-            //const btns = wrapper.find('.btn')
-            //expect(btns.length).toEqual(2);
+      //const btns = wrapper.find('.btn')
+      //expect(btns.length).toEqual(2);
 
-            const closeButton = wrapper.find('.button-close-modal');
-            expect(closeButton.length).toEqual(1);
-            closeButton.simulate('click');
+      const closeButton = wrapper.find('.button-close-modal');
+      expect(closeButton.length).toEqual(1);
+      closeButton.simulate('click');
 
-            //a
-            //.PhoneInputCountrySelect
+      //a
+      //.PhoneInputCountrySelect
 
+      //const buttons = wrapper.find('a');
+      //expect(buttons.length).toEqual(3)
+      //expect(labels.first().html()).toStrictEqual('')
 
-            //const buttons = wrapper.find('a');
-            //expect(buttons.length).toEqual(3)
-            //expect(labels.first().html()).toStrictEqual('')
+      //wrapper.find('button').first().simulate('click');
+    });
+  });
+});
 
+describe('SignInModal - No Portal', () => {
+  const oldCreatePortal = ReactDOM.createPortal;
 
-            //wrapper.find('button').first().simulate('click');
-        });
-    })
+  beforeAll(() => {
+    ReactDOM.createPortal = jest.fn((element, node) => {
+      return element as ReactPortal;
+    });
+  });
+
+  afterEach(() => {
+    ReactDOM.createPortal = oldCreatePortal;
+  });
+
+  it('should render correctly', async () => {
+    const showModal = true;
+    const showPhoneNumber = false;
+    const showOTP = false;
+
+    const setShowModal = jest.fn();
+    const setPhoneNumber = jest.fn();
+    const setOTPNumber = jest.fn();
+    const setShowPhoneNumber = jest.fn();
+    const setConfirmResult = jest.fn();
+    const setShowOTP = jest.fn();
+
+    const login = jest.fn();
+    const confirmResult = jest.fn();
+
+    const { container, getAllByPlaceholderText, getAllByText } = render(
+      <SignInModal
+        showModal={showModal}
+        showPhoneNumber={showPhoneNumber}
+        showOTP={showOTP}
+        setShowModal={setShowModal}
+        setPhoneNumber={setPhoneNumber}
+        setOTPNumber={setOTPNumber}
+        setShowPhoneNumber={setShowPhoneNumber}
+        setConfirmResult={setConfirmResult}
+        setShowOTP={setShowOTP}
+        phoneNumber={''}
+        otpNumber={''}
+        login={login}
+        confirmResult={confirmResult}
+      />,
+      {
+        container: document.body,
+      },
+    );
+
+    await act(async () => {
+      const input = getAllByPlaceholderText('Enter phone number');
+      expect(input.length).toBe(1);
+
+      input.at(0)?.focus();
+      input.at(0)?.blur();
+      fireEvent.keyPress(input.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+      fireEvent.keyDown(input.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+      fireEvent.keyUp(input.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+
+      const input2 = getAllByPlaceholderText('e.g., 123456');
+      expect(input2.length).toBe(1);
+
+      input2.at(0)?.focus();
+      input2.at(0)?.blur();
+      fireEvent.keyPress(input2.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+      fireEvent.keyDown(input2.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+      fireEvent.keyUp(input2.at(0) as HTMLElement, { key: '1', keyCode: 49, which: 49 });
+
+      const button1 = getAllByText('Enter Number');
+      expect(button1.length).toBe(1);
+      button1.at(0)?.click();
+
+      const button2 = getAllByText('Enter OTP');
+      expect(button2.length).toBe(1);
+      button2.at(0)?.click();
+
+      fireEvent.click(document.body);
+    });
+
+    //expect(getAllByPlaceholderText('Enter phone number').at(0)?.innerText).toBe('1');
+
+    //expect(container.innerHTML).toStrictEqual('asdf');
+  });
 });
